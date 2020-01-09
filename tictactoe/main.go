099b1ac984096580481ec90cced8f261072d8170
JesseyBlan <jesseybland@gmail.com>
2020-01-08 19:40:31 -0600

    I've refactored the grid statement in the code in a for loop to clean it up also I"ve added the init function initializing the grid array.

--- tictactoe/main.go
+++ tictactoe/main.go
@@ -6,36 +6,45 @@ import (
 	"time"
 )
 
+var grid [9]string
+var x int
+
+func makeGrid() { //Prints the 9x9 grid
+	for i := 0; i < 9; i++ {
+		fmt.Print(grid[i])
+	}
+}
+
+func init() {
+	grid = [9]string{"[1]", "[2]", "[3]\n", "[4]", "[5]", "[6]\n", "[7]", "[8]", "[9]\n"}
+
+}
+
 func main() {
-	//the tic tax toe grid array
-	map1 := [9]string{"[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]", "[9]"}
-	var x int
-	//Nested for loop first loop asks for x placement the second loop is the ai move
+
 	for i := 0; i < 5; i++ {
-		fmt.Println(map1[0], map1[1], map1[2])
-		fmt.Println(map1[3], map1[4], map1[5])
-		fmt.Println(map1[6], map1[7], map1[8])
+		makeGrid()
 		fmt.Println("Your move pick 1-9")
 		fmt.Scan(&x)
 		switch {
-		case x == 1 && map1[0] != "[X]" && map1[0] != "[O]":
-			map1[0] = "[X]"
-		case x == 2 && map1[1] != "[X]" && map1[1] != "[O]":
-			map1[1] = "[X]"
-		case x == 3 && map1[2] != "[X]" && map1[2] != "[O]":
-			map1[2] = "[X]"
-		case x == 4 && map1[3] != "[X]" && map1[3] != "[O]":
-			map1[3] = "[X]"
-		case x == 5 && map1[4] != "[X]" && map1[4] != "[O]":
-			map1[4] = "[X]"
-		case x == 6 && map1[5] != "[X]" && map1[5] != "[O]":
-			map1[5] = "[X]"
-		case x == 7 && map1[6] != "[X]" && map1[6] != "[O]":
-			map1[6] = "[X]"
-		case x == 8 && map1[7] != "[X]" && map1[7] != "[O]":
-			map1[7] = "[X]"
-		case x == 9 && map1[8] != "[X]" && map1[8] != "[O]":
-			map1[8] = "[X]"
+		case x == 1 && grid[0] != "[X]" && grid[0] != "[O]":
+			grid[0] = "[X]"
+		case x == 2 && grid[1] != "[X]" && grid[1] != "[O]":
+			grid[1] = "[X]"
+		case x == 3 && grid[2] != "[X]\n" && grid[2] != "[O]\n":
+			grid[2] = "[X]\n"
+		case x == 4 && grid[3] != "[X]" && grid[3] != "[O]":
+			grid[3] = "[X]"
+		case x == 5 && grid[4] != "[X]" && grid[4] != "[O]":
+			grid[4] = "[X]"
+		case x == 6 && grid[5] != "[X]\n" && grid[5] != "[O]\n":
+			grid[5] = "[X]\n"
+		case x == 7 && grid[6] != "[X]" && grid[6] != "[O]":
+			grid[6] = "[X]"
+		case x == 8 && grid[7] != "[X]" && grid[7] != "[O]":
+			grid[7] = "[X]"
+		case x == 9 && grid[8] != "[X]\n" && grid[8] != "[O]\n":
+			grid[8] = "[X]\n"
 		default:
 			fmt.Println("Error: The Number you entered has been chosen or you didn't enter a number.")
 			i--
@@ -52,9 +61,7 @@ func main() {
 			n := rand.Intn(10)
 			//This checks to see if the game is finished 5 x moves only 4 circle moves
 			if i > 3 {
-				fmt.Println(map1[0], map1[1], map1[2])
-				fmt.Println(map1[3], map1[4], map1[5])
-				fmt.Println(map1[6], map1[7], map1[8])
+				makeGrid()
 				fmt.Println("You Finished!")
 				y++
 			} else {
@@ -62,32 +69,32 @@ func main() {
 				//on if the grid is used already it loops back and trys a new random number
 				switch {
 
-				case n == 1 && map1[0] != "[X]" && map1[0] != "[O]":
-					map1[0] = "[O]"
+				case n == 1 && grid[0] != "[X]" && grid[0] != "[O]":
+					grid[0] = "[O]"
 					y++
-				case n == 2 && map1[1] != "[X]" && map1[1] != "[O]":
-					map1[1] = "[O]"
+				case n == 2 && grid[1] != "[X]" && grid[1] != "[O]":
+					grid[1] = "[O]"
 					y++
-				case n == 3 && map1[2] != "[X]" && map1[2] != "[O]":
-					map1[2] = "[O]"
+				case n == 3 && grid[2] != "[X]\n" && grid[2] != "[O]\n":
+					grid[2] = "[O]\n"
 					y++
-				case n == 4 && map1[3] != "[X]" && map1[3] != "[O]":
-					map1[3] = "[O]"
+				case n == 4 && grid[3] != "[X]" && grid[3] != "[O]":
+					grid[3] = "[O]"
 					y++
-				case n == 5 && map1[4] != "[X]" && map1[4] != "[O]":
-					map1[4] = "[O]"
+				case n == 5 && grid[4] != "[X]" && grid[4] != "[O]":
+					grid[4] = "[O]"
 					y++
-				case n == 6 && map1[5] != "[X]" && map1[5] != "[O]":
-					map1[5] = "[O]"
+				case n == 6 && grid[5] != "[X]\n" && grid[5] != "[O]\n":
+					grid[5] = "[O]\n"
 					y++
-				case n == 7 && map1[6] != "[X]" && map1[6] != "[O]":
-					map1[6] = "[O]"
+				case n == 7 && grid[6] != "[X]" && grid[6] != "[O]":
+					grid[6] = "[O]"
 					y++
-				case n == 8 && map1[7] != "[X]" && map1[7] != "[O]":
-					map1[7] = "[O]"
+				case n == 8 && grid[7] != "[X]" && grid[7] != "[O]":
+					grid[7] = "[O]"
 					y++
-				case n == 9 && map1[8] != "[X]" && map1[8] != "[O]":
-					map1[8] = "[O]"
+				case n == 9 && grid[8] != "[X]\n" && grid[8] != "[O]\n":
+					grid[8] = "[O]\n"
 					y++
 
 				}
