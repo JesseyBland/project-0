1a8f53e69f8f1593107a18ba40ae7a8c0cdc0cae
JesseyBlan <jesseybland@gmail.com>
2020-01-18 22:48:05 -0600

    Finished Player vs AI HTTP

--- tictactoe/gamehttp/tictactoed.go
+++ tictactoe/gamehttp/tictactoed.go
@@ -19,63 +19,71 @@ func Number(w http.ResponseWriter, r *http.Request) {
 	gameboard.LoadCells("[", "]")
 
 	t, _ := template.ParseFiles("test.html")
-	t2, _ := template.ParseFiles("ttt1.html")
 
-	Cells := struct {
-		One   string
-		Two   string
-		Three string
-		Four  string
-		Five  string
-		Six   string
-		Seven string
-		Eight string
-		Nine  string
-	}{
-		One:   fmt.Sprintf("%v\n", gameboard.Board[0]),
-		Two:   fmt.Sprintf("%v\n", gameboard.Board[1]),
-		Three: fmt.Sprintf("%v\n", gameboard.Board[2]),
-		Four:  fmt.Sprintf("%v\n", gameboard.Board[3]),
-		Five:  fmt.Sprintf("%v\n", gameboard.Board[4]),
-		Six:   fmt.Sprintf("%v\n", gameboard.Board[5]),
-		Seven: fmt.Sprintf("%v\n", gameboard.Board[6]),
-		Eight: fmt.Sprintf("%v\n", gameboard.Board[7]),
-		Nine:  fmt.Sprintf("%v\n", gameboard.Board[8]),
+	type Cells struct {
+		Cwin bool
+		Wonc string
 	}
-	t.Execute(w, Cells)
+	c := Cells{
 
+		Cwin: gamewin.CheckWin(),
+		Wonc: gamewin.WonChat,
+	}
+
+	t.Execute(w, c)
 	r.ParseForm()
 	xread := strings.Join(r.Form["quantity"], "")
 	x, _ := strconv.Atoi(xread)
+	xm := 0
+	om := 0
 
-	if xread != "" {
+	if x == 0 && gameboard.Board[x].Fill == false {
+		fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[0], gameboard.Board[1], gameboard.Board[2])
+		fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[3], gameboard.Board[4], gameboard.Board[5])
+		fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[6], gameboard.Board[7], gameboard.Board[8])
+
+	} else {
+
 		for i := 0; i < len(gameboard.Board); i++ {
 			if x == (i+1) && gameboard.Board[i].Fill == false {
 				gameboard.Board[i].Slogic = "X"
 				gameboard.Board[i].Fill = true
 				gamewin.Moves++
-
+				xm++
+				fmt.Printf("X moves to %v\nTotal moves: %v\n", x, gamewin.Moves)
 				break
 			}
+
 		}
+
 		gamewin.CheckWin()
-		if gamewin.CheckWin() == false {
+		if gamewin.CheckWin() == false && xm > om {
 			ai.AiOmove()
+			om++
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[0], gameboard.Board[1], gameboard.Board[2])
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[3], gameboard.Board[4], gameboard.Board[5])
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[6], gameboard.Board[7], gameboard.Board[8])
+
 			gamewin.CheckWin()
+
 		} else {
-			fmt.Print(gameboard.PrintBoard())
-			fmt.Fprintln(w, gamewin.WonChat)
-			t2.Execute(w, nil)
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[0], gameboard.Board[1], gameboard.Board[2])
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[3], gameboard.Board[4], gameboard.Board[5])
+			fmt.Fprintf(w, "<p1>%v%v%v <br></p1>", gameboard.Board[6], gameboard.Board[7], gameboard.Board[8])
+		}
+		if gamewin.CheckWin() == true {
+			fmt.Fprintf(w, "<br><p1>%v</p1>", gamewin.WonChat)
+			fmt.Println(gamewin.WonChat)
 
 		}
 	}
-
 }
 
 func main() {
-	fmt.Println("Server Status:Listening Host:localhost Port:9090")
+	fmt.Println("Server Status:Listening Host:localhost Port:8080")
 	http.HandleFunc("/ttt1", Number)
-	err := http.ListenAndServe(":9090", nil) // setting listening port
+
+	err := http.ListenAndServe(":8081", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
